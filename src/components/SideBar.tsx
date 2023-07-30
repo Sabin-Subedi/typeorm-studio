678163e007956de6e6b881a8e4b51501fd6117d1
Sabin Subedi <sabinsubedi125@gmail.com>
2023-07-30 10:26:27 +0545

    initial commits

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+
+import { Button } from "./ui/button";
+
+interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const tables = [
+  {
+    name: "Users",
+  },
+];
+
+export function Sidebar({ className }: SidebarProps) {
+  return (
+    <div className={cn("pb-12 h-full", className)}>
+      <div className="h-full space-y-4 py-4">
+        <div className="py-2">
+          <h2 className="relative px-7 text-lg font-semibold tracking-tight">
+            Tables
+          </h2>
+          <ScrollArea className="h-full px-1">
+            <div className="space-y-1 p-2">
+              {tables?.map((table, i) => (
+                <Button
+                  key={`${table.name}-${i}`}
+                  variant="ghost"
+                  className="w-full justify-start font-normal"
+                >
+                  {table.name}
+                </Button>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+      </div>
+    </div>
+  );
+}
